f3bfc200587281cb21472afd918024069082fd77
Anderson Lobo Feitosa <andersonlf@gmail.com>
2017-09-05 19:07:19 -0300

    changing the name

--- classist.rb
+++ classist.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class classist < Formula
+class Classist < Formula
   homepage "https://github.com/andersonlfeitosa/classist"
   url "https://raw.githubusercontent.com/andersonlfeitosa/homebrew-classist/master/classist-dist.zip"
   sha256 "00f4c849842829b087eb0363c5fae141693e34f9b69c557cf513f353cfdeae44"
